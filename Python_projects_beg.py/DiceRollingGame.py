02c765413d3ff9d1f3f85ae06e6a39a904dc35ed
Angelina <angelina20040523@gmail.com>
2025-07-20 20:08:45 +0200

    Rolling dice progect add improvement the project

--- Python_projects_beg.py/DiceRollingGame.py
+++ Python_projects_beg.py/DiceRollingGame.py
@@ -1,16 +1,24 @@
 from random import randint
-question=input("Roll the dice? (y/n):")
-
+question = input("Roll the dice? (y/n):").lower()
+if question == 'y':
+    times=int(input("How many dice you want to roll?(number):"))
+count_times=0
+rolled_dice=0
 while question!='n':
-
-    if question == 'y': 
-        first = randint(1,6)
-        second = randint(1,6)
-        print(second,first) 
-    
+    if question == 'y':
+        rolled_dice+=1
+        while count_times<times: 
+            first = randint(1,6)
+            second = randint(1,6)
+            print(second,first) 
+            count_times+=1
+            
     else:
         print("Invalid choice!")
-        
-    question = input("Roll the dice? (y/n):")
+        question = input("Roll the dice? (y/n):").lower()
 
+    count_times=0
+    question = input("Roll the dice? (y/n):").lower()
+    times=int(input("How many dice you want to roll?(number):"))
+print(f"You rolled the dice {rolled_dice:*^11d} times")
 print("Thanks for playing")
