d3307efdd0b56fc05ce809f80d554619c2ac9a76
Angelina <angelina20040523@gmail.com>
2025-07-28 12:54:21 +0200

    add QR code project

--- Python_projects_beg/Qrcode.py
+++ Python_projects_beg/Qrcode.py
@@ -1,2 +1,11 @@
 import qrcode
-img = input('')
+from qrcode.constants import ERROR_CORRECT_H
+
+user_answer = input("Enter URL/text:").strip()
+file_name = input("Enter the name of the file (filename.jpg):").strip()
+qr = qrcode.QRCode(version=1, box_size = 10, border = 5)
+qr.add_data(user_answer)
+qr.make(fit = True)
+image = qr.make_image(fill_color = 'white',back_color = 'black')
+image.save(file_name)
+print(f"QR saved as {file_name}")
